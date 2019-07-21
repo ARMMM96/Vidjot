0e5d472ee2585c582130f087ad01da4331965d4c
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2019-07-21 20:39:38 +0200

    Remove Idea data

--- app.js
+++ app.js
@@ -126,6 +126,13 @@ app.put('/ideas/:id', (req, res) => {
     })
 });
 
+// Delete Idea
+app.delete('/ideas/:id', (req, res) => {
+    Idea.remove({_id: req.params.id})
+    .then(() => {
+        res.redirect('/ideas');
+    })
+});
 
 const port = 5000;
 app.listen(port, () => {

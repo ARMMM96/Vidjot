f815ff6caac61fe4534a22512942f51bb95f185e
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2019-07-22 15:16:54 +0200

    Register form validation

--- routes/users.js
+++ routes/users.js
@@ -15,4 +15,28 @@ router.get('/register', (req, res) =>{
     res.render('users/register');
 });
 
+// Register Form POST
+router.post('/register', (req,res) => {
+    let errors = [];
+    if(req.body.password != req.body.password2){
+        errors.push({text: 'Passwords dont match'});
+    }
+    if(req.body.password.length < 4 ){
+        errors.push({text: 'Passowrd must be at least 4 characters'});
+    }
+
+    if(errors.length > 0){
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.email,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2,
+        });
+    } else {
+        res.send('passed');
+    }
+    
+})
+
 module.exports = router;

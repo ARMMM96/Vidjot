1c4780d64a3d9bfc5dbb3c4f1dd0ae29d8eb5a8b
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2019-07-30 16:26:59 +0200

    Logout function

--- routes/users.js
+++ routes/users.js
@@ -76,9 +76,17 @@ router.post('/register', (req, res) => {
                         });
                     });
                 }
-            })
+            });
     }
 
-})
+});
+
+// Logout User
+router.get('/logout', (req,res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
+
 
 module.exports = router;
